eef507053497d93a0115a0d4c7bf987850ae96b3
Nikita Karpov <bonilka@gmail.com>
2015-08-29 00:06:04 +0300

    Add responsive text in circle

--- main.js
+++ main.js
@@ -2,11 +2,19 @@ console.log("include main.js")
 // window.onload = function() {
     var Canva = function (data) {
         var canvas = data.el;
+        var defaultBorderWidth = function () {
+            var w = data['el'].clientWidth;
+            return w*0.1;
+        }
+        this.textDefault = function () {
+            return 'normal '+ data['el'].clientWidth*0.5 + '% Helvetica, Arial, sans-serif'
+        }
         this.prec = data['value'];
         this.borderColor = data['borderColor'] || 'rgb(41, 180, 194)';
-        this.borderWidth = data['borderWidth'] || '20';
-        this.backgroundColor = data['backgroundColor'] || 'transparent';
+        this.borderWidth = data['borderWidth'] || defaultBorderWidth();
+        this.innerColor = data['innerColor'] || 'transparent';
         this.textColor = data['textColor'] || '#000';
+        this.font = data['font'] || this.textDefault();
         this.context = canvas.getContext("2d");
     }
     Canva.prototype.arc = function (center, radius, start, end, counterClockwise, borderWidth, color) {
@@ -22,54 +30,58 @@ console.log("include main.js")
         // рисуем круг
         this.context.beginPath();
         this.context.arc(center.x, center.y, radius, 0, 2*Math.PI, false);
-        this.context.fillStyle = this.backgroundColor;
+        this.context.fillStyle = this.innerColor;
         this.context.fill();
     };
-    Canva.prototype.text = function (data) {
+    Canva.prototype.printText = function (data) {
+        this.font = data['font'] || this.textDefault();
         this.context.textBaseline = "middle";
         this.context.textAlign = "center";
-        if(data.font){
-            this.context.font = data.font;
-        }else{
-            this.context.font = 'normal 1em Helvetica, Arial, sans-serif'
-        };
+        this.context.font = this.font;
         this.context.fillStyle = this.textColor;
         this.context.fillText(data.text, data.center.x, data.center.y);
     };
     // var canva = new Canva(document.getElementById('pie-0'));
     // canva.arc();
     var Pie = function (data) {
+        var that = this;
         this.parentEl = document.getElementById(data['id']);
         this.createEl();
+        console.log(this.w);
         this.calcCenter();
-        console.log(this.el);
-        data.el = this.el; //добавлям элемент в объект настроек
+        data.el = this.el; //add in settings object
+        this.text = data['text']; //print text in center (val%) ?
         Canva.call(this, data);
         this.print();
+        window.addEventListener('resize', function () {
+            if(that.parentEl.clientWidth > that.w || that.parentEl.clientWidth < that.w){
+                that.setSize();
+                that.print();
+                if(this.text){
+                    this.printText({text: this.prec+'%', center: this.center});
+                }
+                console.log('is resize!');
+            }
+        })
     }
     Pie.prototype = Object.create(Canva.prototype);
     Pie.prototype.constructor = Canva;
     Pie.prototype.createEl = function () {
         this.el = document.createElement('canvas');
+        this.setSize();
         this.parentEl.appendChild(this.el);
-        this.getWidth();
-        this.getHeight();
+        this.el.style.display = 'block';
+    };
+    Pie.prototype.setSize = function () {
         this.setHeight();
         this.setWidth();
-
     };
     Pie.prototype.setHeight = function () {
-        //this.el.style.height = this.h+'px';
-        // console.log(this.el);
+        this.h = this.parentEl.clientWidth;
         this.el.setAttribute('height', this.h);
     }
-    Pie.prototype.getHeight = function () {
-        this.h = this.parentEl.clientWidth;
-    }
-    Pie.prototype.getWidth = function () {
-        this.w = this.parentEl.clientWidth;
-    }
     Pie.prototype.setWidth = function () {
+        this.w = this.parentEl.clientWidth;
         this.el.setAttribute('width', this.w)
     }
     Pie.prototype.calcCenter = function () {
@@ -78,35 +90,17 @@ console.log("include main.js")
         this.center.y  = this.h/2;
     }
     Pie.prototype.calcArcRad = function () {
-        return this.prec*2*Math.PI/100 + 1.5*Math.PI; //+ 1.5*PI, тк отстчет начинается 1.5*PI, а не с нуля
+        return this.prec*2*Math.PI/100 + 1.5*Math.PI; //+ 1.5*PI, because start in 1.5*PI, not in 0
     };
     Pie.prototype.print = function () {
+        this.calcCenter();
         this.arc(this.center,  this.h/2-this.borderWidth/2, 1.5*Math.PI, this.calcArcRad(), false, this.borderWidth);
         this.round(this.center,  this.h/2-this.borderWidth);
-        this.text({text: this.prec+'%', center: this.center});
+        if(this.text){
+            this.printText({text: this.prec+'%', center: this.center});
+        }
     };
-    var pie = new Pie({'id':'pie-1', 'value': 99, 'font': '', 'borderWidth': '20', 'textColor': '#c0c0c0',  'innerColor': '#0429ff', 'backgroundColor': ''})
+    var pie = new Pie({'id':'pie-1', 'value': 95, 'font': '', 'borderWidth': '20', 'textColor': '#c0c0c0',  'innerColor': '', 'text': 'true'});
     // {'id':'pie-1', 'value': 35, 'font': '', 'borderWidth': '20', 'textColor': '#c0c0c0', 'borderColor': '#a0ffff', 'innerColor': '#0429ff', 'backgroundColor'}
-    // pie.setHeight();
-    // pie.getCenter()
-    // console.dir(pie);
-
-    // var PI = Math.PI
-    // var canvas = document.getElementById("pie");
-    // if (canvas.getContext){
-    //     var context = canvas.getContext("2d");
-    //
-    //     context.beginPath();
-    //     // context.moveTo(150, 100);
-    //     // context.lineTo(250, 50);
-    //     context.lineWidth = 10;
-    //     context.strokeStyle = "#aa0000";
-    //     context.arc(150, 100, 50, 1.5*PI, 2*PI, false);
-    //     // context.lineCap = "round";
-    //     context.stroke();
-    // }
-    // else {
-    //     alert('Canvas not supported')
-    // }
 
 // };
